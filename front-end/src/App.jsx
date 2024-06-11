f99b6799a039e5a7bb36c842300f43b67e834693
ngan <vutran@sheridancollege.ca>
2024-06-11 15:04:30 -0400

    Proxy Setup

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,9 +1,28 @@
+import { useEffect } from "react"
+
 function App() {
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        let res = await fetch("/api/expenses")
+        if (!res.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        let data = await res.json()
+        console.log(data)
+      } catch (error) {
+        console.error('There was a problem with your fetch operation:', error)
+      }
+    }
+    fetchData()
+  }, [])
+  
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <h1 className="text-3xl font-bold underline">Hello world!</h1>
   )
+
 }
 
 export default App
