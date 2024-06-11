a291c2e283d52cc9344cb5874c798b97322f0c32
ngan <vutran@sheridancollege.ca>
2024-06-11 16:15:51 -0400

    DELETE is done

--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -4,6 +4,18 @@ export function Home() {
 
     let [expenses, setExpenses] = useState(null)
 
+    async function removeFromDb(id) {
+        let result = await fetch(`/api/expenses/${id}`, {
+            method: "DELETE"
+        })
+        let data = await result.json()
+        console.log(data)
+    }
+
+    async function handleDeleteBtn(e) {
+        await removeFromDb(e.target.dataset.id)
+    }
+
     useEffect(() => {
         async function fetchData() {
           try {
@@ -24,7 +36,8 @@ export function Home() {
                 expenses.map(e => (
                     <div key={e._id} className="mb-2">
                         <p>Transaction Type: {e.transactionType}</p>    
-                        <p>Price: ${e.price}</p>    
+                        <p>Price: ${e.price}</p>   
+                        <button className="bg-zinc-300 px-4 py-1 rounded-md" onClick={handleDeleteBtn} data-id={e._id}>Delete</button> 
                     </div>
                 ))
             }
