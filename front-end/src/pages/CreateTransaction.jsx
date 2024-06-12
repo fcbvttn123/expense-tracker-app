dc835e3531c5f437f92ea27c0f91391b590a4696
ngan <vutran@sheridancollege.ca>
2024-06-12 10:44:00 -0400

    Form is styled

--- front-end/src/pages/CreateTransaction.jsx
+++ front-end/src/pages/CreateTransaction.jsx
@@ -2,7 +2,31 @@ import { useState } from "react"
 import { handleFormChange } from "../hooks/handleFormChange"
 import { addToMongoDb } from "../hooks/addToMongoDb"
 
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles((theme) => ({
+    form: {
+      '& > *': {
+        margin: theme.spacing(1),
+        width: '25ch',
+      },
+      paddingTop: 80,
+      paddingLeft: 10,
+      display: "flex",
+      flexDirection: "column"
+    },
+    btn: {
+        '& > *': {
+            margin: theme.spacing(1),
+        },
+    },
+  }))
+
 export function CreateTransaction() {
+
+    const classes = useStyles()
 
     const [formData, setFormData] = useState({
         transactionType: "",
@@ -12,25 +36,28 @@ export function CreateTransaction() {
 
     return (
         <form 
-            className="pt-20 pl-5"
             onSubmit={(e) => {
                 e.preventDefault()
                 addToMongoDb(formData, "/api/expenses")
             }}
+            className={classes.form} 
+            noValidate 
+            autoComplete="off"
         >
-            <div className="flex items-center gap-4 mb-2">
-                <label htmlFor="transactionType" className="min-w-32">Transaction Type</label>
-                <input type="text" name="transactionType" id="transactionType" value={formData.transactionType} onChange={(e) => handleFormChange(e, setFormData)} className="border-2 border-black" />
-            </div>
-            <div className="flex items-center gap-4 mb-2">
-                <label htmlFor="price" className="min-w-32">Price</label>
-                <input type="number" name="price" id="price" value={formData.price} onChange={(e) => handleFormChange(e, setFormData)} className="border-2 border-black" />
-            </div>
-            <div>
-                <label htmlFor="notes" className="min-w-32">Notes</label>
-                <textarea name="notes" id="notes" value={formData.notes} onChange={(e) => handleFormChange(e, setFormData)} className="border-2 border-black"></textarea>
-            </div>
-            <button className="px-8 py-2 bg-slate-300">Add</button>
+            <TextField id="filled-basic" label="Transaction Type" variant="filled" name="transactionType" value={formData.transactionType} onChange={(e) => handleFormChange(e, setFormData)} />
+            <TextField id="filled-basic" label="Price" variant="filled" name="price" value={formData.price} type="number" onChange={(e) => handleFormChange(e, setFormData)} />
+            <TextField
+                id="filled-multiline-static"
+                label="Multiline"
+                multiline
+                minRows={4}
+                defaultValue="Default Value"
+                variant="filled"
+                name="notes"
+                value={formData.notes} 
+                onChange={(e) => handleFormChange(e, setFormData)}
+            />
+            <Button variant="contained" color="primary"> Add </Button>
         </form>
     )
 
